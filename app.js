23a1bc947fe1dfd95ad51350814f3c7a259a564a
vivektech1505 <vivektech1505@gmail.com>
2024-01-10 00:37:34 +0530

    Updated

---
+++ app.js
@@ -0,0 +1,5 @@
+const heading = React.createElement("h1", {className : "heading"}, "Welcome to my React js Classes");
+
+const vivek = ReactDOM.createRoot(document.getElementById("root"));
+
+vivek.render(heading)
