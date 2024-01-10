6d8ffefab4a9757e920d3c4dfbbafa20d674785c
vivektech1505 <vivektech1505@gmail.com>
2024-01-10 14:11:32 +0530

    again updated

---
+++ dist/index.7c0ccee6.js
@@ -0,0 +1,7 @@
+const heading = React.createElement("h1", {
+    className: "heading"
+}, "Welcome to my React js Classes");
+const vivek = ReactDOM.createRoot(document.getElementById("root"));
+vivek.render(heading);
+
+//# sourceMappingURL=index.7c0ccee6.js.map
