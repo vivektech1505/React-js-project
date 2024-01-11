1008b1af687ea3f34feb679efb2db9799c00938c
vivektech1505 <vivektech1505@gmail.com>
2024-01-11 14:28:45 +0530

    updated

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
 
-const heading = React.createElement("h1", {className : "heading"}, "Welcome to my React js Classes");
+// React.createElement => Object => HTMLElement(render)
+
+const heading = React.createElement("h1",{id:"heading"}, "Namaste React By Fake Er. Vivek Kumar");
+
+// JSX - is not HTML in side javascrit,
+// its html like or Xml like syntax
+// jsx is just a syntax
+// JSX syntax is different and React syntax is difference
+
+console.log(heading)
+
+const jsxHeading = <h1 id="heading">Namste React using JSX </h1>
+console.log(jsxHeading)
 
 const vivek = ReactDOM.createRoot(document.getElementById("root"));
 
